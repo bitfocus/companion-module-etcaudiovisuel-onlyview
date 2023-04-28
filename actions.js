f89e573edad7848f9f03ccedb30bce421010ccb1
Guillaume Breda <breda.guillaume@gmail.com>
2023-04-28 15:49:38 +0200

    [ACTION] Refactor sending TCP messages

--- actions.js
+++ actions.js
@@ -1,4 +1,18 @@
 module.exports = function (self) {
+    function sendTcp(str) {
+        /* Create a binary buffer pre-encoded 'latin1' (8bit no change bytes) sending a string.
+         * Assumes 'utf8' encoding which then escapes character values over 0x7F and destroys
+         * the 'binary' context.
+         */
+        const buffer = Buffer.from(str, 'latin1')
+
+        if (self.socket !== undefined && self.socket.isConnected) {
+            self.socket.send(buffer)
+        } else {
+            self.log('debug', 'Socket not connected :(')
+        }
+    }
+
     self.setActionDefinitions({
         timelinePlayAction: {
             name: 'Timeline play',
@@ -14,17 +28,8 @@ module.exports = function (self) {
                 },
             ],
             callback: async (action) => {
-                /* Create a binary buffer pre-encoded 'latin1' (8bit no change bytes) sending a string.
-                 * Assumes 'utf8' encoding which then escapes character values over 0x7F and destroys
-                 * the 'binary' context.
-                 */
-                const buffer = Buffer.from('play*' + action.options.timelineId + '\n', 'latin1')
-
-                if (self.socket !== undefined && self.socket.isConnected) {
-                    self.socket.send(buffer)
-                } else {
-                    self.log('debug', 'Socket not connected :(')
-                }
+                const str = 'play*' + action.options.timelineId + '\n'
+                sendTcp(str)
             },
         },
 
@@ -42,13 +47,8 @@ module.exports = function (self) {
                 },
             ],
             callback: async(action) => {
-                const buffer = Buffer.from('locate*' + action.options.timelineId + '\n', 'latin1')
-
-                if (self.socket !== undefined && self.socket.isConnected) {
-                    self.socket.send(buffer)
-                } else {
-                    self.log('debug', 'Socket not connected :(')
-                }
+                const str = 'locate*' + action.options.timelineId + '\n'
+                sendTcp(str)
             },
         },
     })
