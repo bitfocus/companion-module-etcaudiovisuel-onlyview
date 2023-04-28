2743e065619b7dd9499234e4bc5a4e417cabffbb
Guillaume Breda <breda.guillaume@gmail.com>
2023-04-28 15:35:57 +0200

    [STYLE] Replace tabs with 4 spaces

--- actions.js
+++ actions.js
@@ -1,31 +1,31 @@
 module.exports = function (self) {
-	self.setActionDefinitions({
-		timelinePlayAction: {
-			name: 'Timeline play',
-			options: [
-				{
-					type: 'number',
-					id: 'timelineId',
-					label: 'Timeline Id',
-					default: '1',
-					min: 1,
-					max: 99,
-					useVariables: true,
-				},
-			],
-			callback: async (action) => {
-				/* Create a binary buffer pre-encoded 'latin1' (8bit no change bytes) sending a string.
-				 * Assumes 'utf8' encoding which then escapes character values over 0x7F and destroys
-				 * the 'binary' context.
-				 */
-				const buffer = Buffer.from('play*' + action.options.timelineId + '\n', 'latin1')
+    self.setActionDefinitions({
+        timelinePlayAction: {
+            name: 'Timeline play',
+            options: [
+                {
+                    type: 'number',
+                    id: 'timelineId',
+                    label: 'Timeline Id',
+                    default: '1',
+                    min: 1,
+                    max: 99,
+                    useVariables: true,
+                },
+            ],
+            callback: async (action) => {
+                /* Create a binary buffer pre-encoded 'latin1' (8bit no change bytes) sending a string.
+                 * Assumes 'utf8' encoding which then escapes character values over 0x7F and destroys
+                 * the 'binary' context.
+                 */
+                const buffer = Buffer.from('play*' + action.options.timelineId + '\n', 'latin1')
 
-				if (self.socket !== undefined && self.socket.isConnected) {
-					self.socket.send(buffer)
-				} else {
-					self.log('debug', 'Socket not connected :(')
-				}
-			},
-		},
-	})
+                if (self.socket !== undefined && self.socket.isConnected) {
+                    self.socket.send(buffer)
+                } else {
+                    self.log('debug', 'Socket not connected :(')
+                }
+            },
+        },
+    })
 }
--- main.js
+++ main.js
@@ -5,93 +5,93 @@ const UpdateFeedbacks = require('./feedbacks')
 const UpdateVariableDefinitions = require('./variables')
 
 class ModuleInstance extends InstanceBase {
-	constructor(internal) {
-		super(internal)
-	}
+    constructor(internal) {
+        super(internal)
+    }
 
-	async init(config) {
-		this.updateStatus(InstanceStatus.Ok)
+    async init(config) {
+        this.updateStatus(InstanceStatus.Ok)
 
-		this.updateActions()
-		this.updateFeedbacks()
-		this.updateVariableDefinitions()
+        this.updateActions()
+        this.updateFeedbacks()
+        this.updateVariableDefinitions()
 
-		await this.configUpdated(config)
-	}
+        await this.configUpdated(config)
+    }
 
-	async destroy() {
-		if (this.socket) {
-			this.socket.destroy()
-		} else {
-			this.updateStatus(InstanceStatus.Disconnected)
-		}
-	}
+    async destroy() {
+        if (this.socket) {
+            this.socket.destroy()
+        } else {
+            this.updateStatus(InstanceStatus.Disconnected)
+        }
+    }
 
-	async configUpdated(config) {
-		if (this.socket) {
-			this.socket.destroy()
-			delete this.socket
-		}
+    async configUpdated(config) {
+        if (this.socket) {
+            this.socket.destroy()
+            delete this.socket
+        }
 
-		this.config = config
-		this.initTcp()
-	}
+        this.config = config
+        this.initTcp()
+    }
 
-	// Return config fields for web config
-	getConfigFields() {
-		return [
-			{
-				type: 'textinput',
-				id: 'host',
-				label: 'Producer IP',
-				width: 8,
-				regex: Regex.IP,
-			},
-			{
-				type: 'textinput',
-				id: 'port',
-				label: 'Producer Port',
-				width: 4,
-				regex: Regex.PORT,
-			},
-		]
-	}
+    // Return config fields for web config
+    getConfigFields() {
+        return [
+            {
+                type: 'textinput',
+                id: 'host',
+                label: 'Producer IP',
+                width: 8,
+                regex: Regex.IP,
+            },
+            {
+                type: 'textinput',
+                id: 'port',
+                label: 'Producer Port',
+                width: 4,
+                regex: Regex.PORT,
+            },
+        ]
+    }
 
-	updateActions() {
-		UpdateActions(this)
-	}
+    updateActions() {
+        UpdateActions(this)
+    }
 
-	updateFeedbacks() {
-		UpdateFeedbacks(this)
-	}
+    updateFeedbacks() {
+        UpdateFeedbacks(this)
+    }
 
-	updateVariableDefinitions() {
-		UpdateVariableDefinitions(this)
-	}
+    updateVariableDefinitions() {
+        UpdateVariableDefinitions(this)
+    }
 
-	initTcp() {
-		if (this.socket) {
-			this.socket.destroy()
-			delete this.socket
-		}
+    initTcp() {
+        if (this.socket) {
+            this.socket.destroy()
+            delete this.socket
+        }
 
-		this.updateStatus(InstanceStatus.Connecting)
+        this.updateStatus(InstanceStatus.Connecting)
 
-		if (this.config.host) {
-			this.socket = new TCPHelper(this.config.host, this.config.port)
+        if (this.config.host) {
+            this.socket = new TCPHelper(this.config.host, this.config.port)
 
-			this.socket.on('status_change', (status, message) => {
-				this.updateStatus(status, message)
-			})
+            this.socket.on('status_change', (status, message) => {
+                this.updateStatus(status, message)
+            })
 
-			this.socket.on('error', (err) => {
-				this.updateStatus(InstanceStatus.ConnectionFailure, err.message)
-				this.log('error', 'Network error: ' + err.message)
-			})
-		} else {
-			this.updateStatus(InstanceStatus.BadConfig)
-		}
-	}
+            this.socket.on('error', (err) => {
+                this.updateStatus(InstanceStatus.ConnectionFailure, err.message)
+                this.log('error', 'Network error: ' + err.message)
+            })
+        } else {
+            this.updateStatus(InstanceStatus.BadConfig)
+        }
+    }
 }
 
 runEntrypoint(ModuleInstance, UpgradeScripts)
