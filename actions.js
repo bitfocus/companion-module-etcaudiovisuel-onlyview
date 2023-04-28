80cf4c2a7bc482ee8fa41fbf9da020300f552e7c
Guillaume Breda <breda.guillaume@gmail.com>
2023-04-28 15:27:32 +0200

    [ACTION] Add timeline play action

--- actions.js
+++ actions.js
@@ -1,19 +1,30 @@
 module.exports = function (self) {
 	self.setActionDefinitions({
-		sample_action: {
-			name: 'My First Action',
+		timelinePlayAction: {
+			name: 'Timeline play',
 			options: [
 				{
-					id: 'num',
 					type: 'number',
-					label: 'Test',
-					default: 5,
-					min: 0,
-					max: 100,
+					id: 'timelineId',
+					label: 'Timeline Id',
+					default: '1',
+					min: 1,
+					max: 99,
+					useVariables: true,
 				},
 			],
-			callback: async (event) => {
-				console.log('Hello world!', event.options.num)
+			callback: async (action) => {
+				/* Create a binary buffer pre-encoded 'latin1' (8bit no change bytes) sending a string.
+				 * Assumes 'utf8' encoding which then escapes character values over 0x7F and destroys
+				 * the 'binary' context.
+				 */
+				const buffer = Buffer.from('play*' + action.options.timelineId + '\n', 'latin1')
+
+				if (self.socket !== undefined && self.socket.isConnected) {
+					self.socket.send(buffer)
+				} else {
+					self.log('debug', 'Socket not connected :(')
+				}
 			},
 		},
 	})
