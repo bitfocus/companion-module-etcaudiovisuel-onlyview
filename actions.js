3a714710b8c0cf2f53f0c327e561f4057246633d
Guillaume Breda <breda.guillaume@gmail.com>
2023-05-09 19:54:18 +0200

    [CLEAN] Group action definitions + comments

--- actions.js
+++ actions.js
@@ -1,4 +1,5 @@
 module.exports = function (self) {
+    // Sends a network message using the TCP socket.
     function sendTcp(str) {
         /* Create a binary buffer pre-encoded 'latin1' (8bit no change bytes) sending a string.
          * Assumes 'utf8' encoding which then escapes character values over 0x7F and destroys
@@ -13,6 +14,7 @@ module.exports = function (self) {
         }
     }
 
+    // Creates a number option giving the desired parameters.
     function numberOption(idStr, labelStr, minValue, maxValue, defaultValue) {
         return {
             type: 'number',
@@ -25,6 +27,7 @@ module.exports = function (self) {
         }
     }
 
+    // Creates a state (boolean) option giving the desired parameters.
     function stateOption(idStr, labelStr) {
         return {
             type: 'checkbox',
@@ -34,6 +37,7 @@ module.exports = function (self) {
         }
     }
 
+    // Creates a drop down option giving the desired parameters.
     function dropDownOption(idStr, labelStr, availableChoices, defaultChoice) {
         return {
             type: 'dropdown',
@@ -45,6 +49,9 @@ module.exports = function (self) {
     }
 
     self.setActionDefinitions({
+
+        // - General actions -
+
         onlineAction: {
             name: 'Online Displays',
             options: [],
@@ -53,6 +60,53 @@ module.exports = function (self) {
                 sendTcp(str)
             },
         },
+
+        quickkeyAction: {
+            name: 'Quick key',
+
+            options: [
+                numberOption('quickkeyId', 'Quick key Id', 1, 999, 1),
+            ],
+            callback: async(action) => {
+                const str = 'launchquickkey*' + action.options.quickkeyId + '\n'
+                sendTcp(str)
+            },
+        },
+
+        testPatternAction: {
+            name: 'Test pattern',
+            options: [
+                dropDownOption('testPatternId', 'Test pattern', [
+                    {id: 0, label: 'None'},
+                    {id: 1, label: 'Small pattern'},
+                    {id: 2, label: 'Horizontal black level'},
+                    {id: 3, label: 'Vertical black level'},
+                    {id: 4, label: 'Tile'},
+                    {id: 5, label: 'Onlyview logo'},
+                    {id: 6, label: 'SMPTE HD color bar'},
+                    {id: 7, label: 'SMPTE SD color bar'},
+                    {id: 8, label: 'OSD'},
+                    {id: 9, label: 'Framerate (CPU)'},
+                    {id: 10, label: 'Framerate (GPU)'},
+                    {id: 11, label: 'White'}],
+                    0)
+            ],
+            callback: async(action) => {
+                const str = 'setpattern*' + action.options.testPatternId + '\n'
+                sendTcp(str)
+            },
+        },
+
+        shutdownMasterAction: {
+            name: 'Shutdown Master',
+            options: [],
+            callback: async(action) => {
+                const str = 'shutdownmaster\n'
+                sendTcp(str)
+            },
+        },
+
+        // - Timeline actions -
 
         timelinePlayAction: {
             name: 'Timeline play',
@@ -85,6 +139,22 @@ module.exports = function (self) {
             callback: async(action) => {
                 const state = action.options.state ? 1 : 0
                 const str = 'settimelineon*' + action.options.timelineId + '*' + state + '\n'
+                sendTcp(str)
+            },
+        },
+
+        timelineGrandMasterAction: {
+            name: 'Timeline grand master',
+            options: [
+                numberOption('timelineId', 'Timeline Id', 1, 99, 1),
+                numberOption('value', 'Grand master value', 0, 100, 100),
+                numberOption('fadeTime', 'Fade time (seconds)', 0, 99, 0),
+            ],
+            callback: async(action) => {
+                // Onlyview expects a fade time value in milliseconds.
+                const fadeTime = action.options.fadeTime * 1000
+                const str = 'setgrandmaster*' + action.options.timelineId + '*' +
+                    action.options.value + '*' + fadeTime + '\n'
                 sendTcp(str)
             },
         },
@@ -132,21 +202,7 @@ module.exports = function (self) {
             },
         },
 
-        timelineGrandMasterAction: {
-            name: 'Timeline grand master',
-            options: [
-                numberOption('timelineId', 'Timeline Id', 1, 99, 1),
-                numberOption('value', 'Grand master value', 0, 100, 100),
-                numberOption('fadeTime', 'Fade time (seconds)', 0, 99, 0),
-            ],
-            callback: async(action) => {
-                // Onlyview expects a fade time value in milliseconds.
-                const fadeTime = action.options.fadeTime * 1000
-                const str = 'setgrandmaster*' + action.options.timelineId + '*' +
-                    action.options.value + '*' + fadeTime + '\n'
-                sendTcp(str)
-            },
-        },
+        // - Board actions -
 
         boardPlayAction: {
             name: 'Board play',
@@ -241,6 +297,22 @@ module.exports = function (self) {
             },
         },
 
+        boardGrandMasterAction: {
+            name: 'Board grand master',
+            options: [
+                numberOption('baoardId', 'Board Id', 1, 99, 1),
+                numberOption('value', 'Grand master value', 0, 100, 100),
+                numberOption('fadeTime', 'Fade time (seconds)', 0, 99, 0),
+            ],
+            callback: async(action) => {
+                // Onlyview expects a fade time value in milliseconds.
+                const fadeTime = action.options.fadeTime * 1000
+                const str = 'setBoardGrandMaster*' + action.options.baoardId + '*' +
+                    action.options.value + '*' + fadeTime + '\n'
+                sendTcp(str)
+            },
+        },
+
         boardLayerOnOffStateAction: {
             name: 'Board layer ON/OFF state',
             options: [
@@ -275,66 +347,5 @@ module.exports = function (self) {
                 sendTcp(str)
             },
         },
-
-        boardGrandMasterAction: {
-            name: 'Board grand master',
-            options: [
-                numberOption('baoardId', 'Board Id', 1, 99, 1),
-                numberOption('value', 'Grand master value', 0, 100, 100),
-                numberOption('fadeTime', 'Fade time (seconds)', 0, 99, 0),
-            ],
-            callback: async(action) => {
-                // Onlyview expects a fade time value in milliseconds.
-                const fadeTime = action.options.fadeTime * 1000
-                const str = 'setBoardGrandMaster*' + action.options.baoardId + '*' +
-                    action.options.value + '*' + fadeTime + '\n'
-                sendTcp(str)
-            },
-        },
-
-        quickkeyAction: {
-            name: 'Quick key',
-
-            options: [
-                numberOption('quickkeyId', 'Quick key Id', 1, 999, 1),
-            ],
-            callback: async(action) => {
-                const str = 'launchquickkey*' + action.options.quickkeyId + '\n'
-                sendTcp(str)
-            },
-        },
-
-        testPatternAction: {
-            name: 'Test pattern',
-            options: [
-                dropDownOption('testPatternId', 'Test pattern', [
-                    {id: 0, label: 'None'},
-                    {id: 1, label: 'Small pattern'},
-                    {id: 2, label: 'Horizontal black level'},
-                    {id: 3, label: 'Vertical black level'},
-                    {id: 4, label: 'Tile'},
-                    {id: 5, label: 'Onlyview logo'},
-                    {id: 6, label: 'SMPTE HD color bar'},
-                    {id: 7, label: 'SMPTE SD color bar'},
-                    {id: 8, label: 'OSD'},
-                    {id: 9, label: 'Framerate (CPU)'},
-                    {id: 10, label: 'Framerate (GPU)'},
-                    {id: 11, label: 'White'}],
-                    0)
-            ],
-            callback: async(action) => {
-                const str = 'setpattern*' + action.options.testPatternId + '\n'
-                sendTcp(str)
-            },
-        },
-
-        shutdownMasterAction: {
-            name: 'Shutdown Master',
-            options: [],
-            callback: async(action) => {
-                const str = 'shutdownmaster\n'
-                sendTcp(str)
-            },
-        }
     })
 }
